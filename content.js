b9270c06b463a8c9e8d31ac0bc185c47df93d099
Jadua1010 <48727939+Jadua1010@users.noreply.github.com>
2023-09-18 20:35:24 +0200

    QOL update
    Added textboxes, caching & jquery support

--- content.js
+++ content.js
@@ -36,3 +36,75 @@ document.addEventListener('DOMContentLoaded', function () {
     overlay.classList.add('show');
     }, 0); // Show after 1 second
 });
+
+function initializeCheckboxes() {
+  // Get all the elements that match your selector
+  const listItems = document.querySelectorAll('.two-weeks-calendar tbody td.day ul li');
+
+  // Loop through each list item and split into left and right sections
+  listItems.forEach(item => {
+    // Get the event_id attribute value
+    const eventId = item.getAttribute('event_id');
+
+    // Check if eventId is null or undefined
+    if (eventId === null || eventId === undefined) {
+      // If eventId is null or undefined, skip further processing for this item
+      return;
+    }
+
+    // Create a new div to wrap the contents
+    const container = document.createElement('div');
+    container.classList.add('todoitem'); // Add a class for styling
+
+    // Create a checkbox element
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    // Check if there's a stored value for this checkbox in local storage based on the event_id
+    const storedValue = localStorage.getItem(`checkbox_${eventId}`);
+
+    // Set the initial state based on the stored value (if available)
+    checkbox.checked = storedValue === 'true';
+
+    // Add an event listener to update local storage when the checkbox state changes
+    checkbox.addEventListener('change', () => {
+      localStorage.setItem(`checkbox_${eventId}`, checkbox.checked);
+
+      // Toggle the 'saturation-effect' class on the li element
+      item.classList.toggle('saturation-effect', checkbox.checked);
+    });
+
+    // Apply the saturation effect to elements that were loaded as checked from cookies
+    if (checkbox.checked) {
+      item.classList.add('saturation-effect');
+    }
+
+    // Append the checkbox to the left section
+    container.appendChild(checkbox);
+
+    // Create a div for the right section (content) and move the content from the original li into it
+    const rightSection = document.createElement('div');
+    rightSection.classList.add('todo-content'); // Add a class for styling
+
+    while (item.firstChild) {
+      rightSection.appendChild(item.firstChild);
+    }
+
+    // Append the container (checkbox + content) to the list item
+    item.innerHTML = ''; // Clear the content of the original li
+    item.appendChild(container);
+    item.appendChild(rightSection);
+
+    // Add the event_id as a data attribute for later reference (optional)
+    item.dataset.event_id = eventId;
+  });
+}
+
+const intervalId = setInterval(function() {
+  const todoItemElement = document.querySelector('.todoitem');
+  if (!todoItemElement) {
+    initializeCheckboxes();
+  }
+}, 1000);
+	
+	
---
+++ runner.js
@@ -0,0 +1,22 @@
+// Get the current page's URL
+const currentPageUrl = window.location.href;
+
+// Check if the Page is Already Cached
+if (localStorage.getItem(currentPageUrl) === null) {
+    // Page is not cached, so let's cache it.
+    // Fetch the page content and store it in localStorage.
+    fetch(currentPageUrl)
+        .then(response => response.text())
+        .then(pageContent => {
+            localStorage.setItem(currentPageUrl, pageContent);
+        })
+        .catch(error => {
+            console.error('Error fetching or caching the page:', error);
+        });
+} else {
+    // Page is already cached, so load it from the cache.
+    const cachedPage = localStorage.getItem(currentPageUrl);
+    // Insert the cached content into the DOM where needed.
+    document.body.innerHTML = cachedPage;
+	console.log('Using cached page');
+}
